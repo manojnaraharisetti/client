6415f6ed72c22ce658d4531cae2b4a8b56ade21b
Naraharisetti Manoj <110677973+manojnaraharisetti@users.noreply.github.com>
2023-02-27 21:07:34 +0530

    change

--- src/App.js
+++ src/App.js
@@ -1,62 +1,56 @@
-import { useState } from 'react';
 import './App.css';
+import { useState } from 'react';
 import axios from 'axios';
+
 function App() {
   const [result,setResult] = useState(null);
-  function getproducts(){
+  function getProducts(){
     axios.get("http://localhost:1420/products",{
         params : {}
-    })
-    .then((response) => {
+    }).then((response)=>{
         console.log(response.data);
         setResult(response.data);
+    }).catch((error)=>{
+        console.log(error)
     })
-    .catch((error)=>{
-        console.log(error);
-    })
-
   }
-
   function deleteProduct(id){
     axios.delete(`http://localhost:1420/delete/${id}`,
         {params : {}
-    })
-    .then((response)=>{
-        console.log(response.data)
-        getproducts();
-    })
-    .catch((error)=>{
+    }).then((response)=>{
+        console.log(response.data);
+        getProducts();
+    }).catch((error)=>{
         console.log(error);
     })
   }
-
-  if(result === null){
-  return (
-    <div className="App">
-      <button onClick={() => getproducts()}>GetProductData </button>
-    </div>
-  );
+  if( result == null ){
+    return (
+        <div className="App">
+        <button onClick={()=>getProducts()}>GetProductData</button>
+        </div>
+    );
   }else{
     return (
         <div className="App">
-            <table border="1">
-                <tr>
-                    <th>Pid</th>
-                    <th>Pname</th>
-                    <th>Price</th>
-                    <th>Actions</th>
-                </tr>
-                {
-                    result.map((obj)=>(
-                        <tr key={obj.id}>
-                            <td key={obj.id}></td>
-                            <td key={obj.name}></td>
-                            <td key={obj.price}></td>
-                            <td ><button onClick={()=>deleteProduct(obj.id)}>Delete Product</button></td>
-                        </tr>
-                    ))
-                }
-            </table>
+          <table border="1">
+            <tr>
+                <th>PID</th>
+                <th>PNAME</th>
+                <th>PRICE</th>
+                <th>ACTIONS</th>
+            </tr>
+            {
+                result.map((obj)=>(
+                    <tr key={obj.id}>
+                        <td>{obj.id}</td>
+                        <td>{obj.name}</td>
+                        <td>{obj.price}</td>
+                        <td><button onClick={()=>deleteProduct(obj.id)}>Delete</button></td>
+                    </tr>
+                ))
+            }
+          </table>
         </div>
       );
   }
